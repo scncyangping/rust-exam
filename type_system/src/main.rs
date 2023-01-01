c1d9e4024a23e4d81a55844846f2c5e5d5dd1068
Yapi <hero.yp@qq.com>
2023-01-01 22:54:36 +0800

    feat(type_system): add type eaxm

---
+++ type_system/src/main.rs
@@ -0,0 +1,27 @@
+use std::io::{BufWriter, Write};
+use std::net::TcpStream;
+
+#[derive(Debug)]
+struct MyWriter<W> {
+    writer: W,
+}
+
+impl<W: Write> MyWriter<W> {
+    pub fn new(w: W) -> Self {
+        Self { writer: w }
+    }
+
+    pub fn write(&mut self, buf: &str) -> std::io::Result<()> {
+        self.writer.write_all(buf.as_bytes())
+    }
+}
+
+fn main() {
+    let stream = TcpStream::connect("127.0.0.1:9090").unwrap();
+    let mut writer = MyWriter::new(stream);
+    println!("stream: {:?}", stream);
+    match writer.write("hello world!") {
+        Ok(_) => println!("ok"),
+        Err(e) => println!("error: {}", e),
+    }
+}
