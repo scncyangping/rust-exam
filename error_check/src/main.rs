b9bff19ada81a69396b2cfef341d564b19d84a99
Yapi <hero.yp@qq.com>
2023-12-27 20:16:31 +0800

    fix(err_check): read_file_chan

--- error_check/src/main.rs
+++ error_check/src/main.rs
@@ -3,6 +3,17 @@ use std::io::Read;
 
 fn main() {}
 
+fn read_file_chan(path: &str) -> Result<String, String> {
+    File::open(path)
+        .map_err(|er| format!("error opening file: {}", er))
+        .and_then(|mut file| {
+            let mut contents = String::new();
+            file.read_to_string(&mut contents)
+                .map_err(|err| format!("Error reading file: {}", err))
+                .map(|_| contents)
+        })
+}
+
 fn read_file(path: &str) -> Result<String, String> {
     match File::open(path).map_err(|err| format!("error open file {}", err)) {
         Ok(mut file) => {
