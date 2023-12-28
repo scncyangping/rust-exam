229092e3dc4ee98af75e92ecc7247f558be03444
Yapi <hero.yp@qq.com>
2023-12-28 10:11:58 +0800

    fix(err_check): thiserror create

--- error_check/src/main.rs
+++ error_check/src/main.rs
@@ -1,8 +1,27 @@
 use std::fs::File;
+use std::io;
 use std::io::Read;
-
+use thiserror::Error;
 fn main() {}
+#[derive(Error, Debug)]
+pub enum SelfError {
+    #[error("data store disconnected")]
+    Disconnect(#[from] io::Error),
+    #[error("the data for key `{0}` is not available")]
+    Redaction(String),
+    #[error("invalid header (expected {expected:?},found {found:?})")]
+    InvalidHeader { expected: String, found: String },
+    #[error("unknown data store error")]
+    Unknown,
+}
 
+fn read_file_simple(path: &str) -> Result<String, String> {
+    let mut file = File::open(path).map_err(|er| format!("error opening file: {}", er))?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .map_err(|err| format!("Error reading file: {}", err))?;
+    Ok(contents)
+}
 fn read_file_chan(path: &str) -> Result<String, String> {
     File::open(path)
         .map_err(|er| format!("error opening file: {}", er))
