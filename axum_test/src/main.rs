48cc600592401724e98f33c81095a20df1a26ae5
Yapi <hero.yp@qq.com>
2024-01-03 10:47:56 +0800

    feat(axum): demo

---
+++ axum_test/src/main.rs
@@ -0,0 +1,18 @@
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
+        .await
+        .unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello,World!</h1>")
+}
