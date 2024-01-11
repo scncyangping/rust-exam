359a133426c5b16c389e1580a48276c120bb2f04
Yapi <hero.yp@qq.com>
2024-01-11 09:18:43 +0800

    feat(lc): merge

--- leet_code/src/main.rs
+++ leet_code/src/main.rs
@@ -3,6 +3,34 @@ use std::collections::HashMap;
 fn main() {
     let rs = two_sum(vec![3, 3], 6);
     println!("{:?}", rs);
+
+    match 1 {
+        num @ (1 | 2) => {
+            println!("{}", num);
+        }
+        _ => {}
+    }
+}
+
+pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
+    let mut m = m as usize;
+    let mut n = n as usize;
+    let mut right = m + n;
+
+    while n > 0 {
+        right -= 1;
+        if m == 0 || nums1[m - 1] < nums2[n - 1] {
+            nums1[right] = nums2[n - 1];
+            if n > 0 {
+                n -= 1
+            }
+        } else {
+            nums1.swap(m - 1, right);
+            if m > 0 {
+                m -= 1
+            }
+        }
+    }
 }
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
