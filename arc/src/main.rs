fc8fb8c84374a2906c22eca91085716d43b1b395
Yapi <hero.yp@qq.com>
2024-02-08 15:13:48 +0800

    fix(as_ref): impl as_ref

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -9,10 +9,12 @@ fn arc_mutex_is_send_sync() {
     let c = a.clone();
 
     let handle = thread::spawn(move || {
+        println!("c is execute");
         let mut g = c.lock().unwrap();
         *g += 1;
     });
     {
+        println!("b is execute");
         let mut g = b.lock().unwrap();
         *g += 1;
     }
@@ -28,6 +30,20 @@ fn main() {
     print(x);
     let y = "Hello world".to_string();
     print(y);
+    let z = SelfStruct {
+        name: String::from("123"),
+    };
+    print(z);
+}
+
+struct SelfStruct {
+    name: String,
+}
+
+impl AsRef<str> for SelfStruct {
+    fn as_ref(&self) -> &str {
+        self.name.as_ref()
+    }
 }
 
 fn print(v: impl AsRef<str>) {
