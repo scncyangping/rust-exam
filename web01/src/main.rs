840f791603cb1140be76be8c9c6e6370bd4dc3ac
Yapi <hero.yp@qq.com>
2024-02-26 14:14:30 +0800

    feat(axum): axum with askama template

--- web01/src/main.rs
+++ web01/src/main.rs
@@ -1,5 +1,8 @@
 mod db;
 
+use askama::Template;
+use axum::extract::Path;
+use axum::response::{Html, IntoResponse, Response};
 use axum::{
     http::StatusCode,
     routing::{get, post},
@@ -17,6 +20,11 @@ async fn main() {
     let pool = db::establish_connection().await;
     // build our application with a route
     let app = Router::new()
+        // 模版路径
+        .route("/tem1", get(template_str))
+        .route("/tem2", get(template_string))
+        .route("/tem3", get(template_include))
+        .route("/tem4/:name", get(askama_template))
         .route("/api/login", post(api::users::login))
         .with_state(pool);
     // run our app with hyper, listening globally on port 3000
@@ -24,4 +32,44 @@ async fn main() {
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn template_string() -> Html<String> {
+    Html("<h1>hello template_string world</h1>".to_string())
+}
+
+async fn template_str() -> Html<&'static str> {
+    Html("<h1>hello template_str world</h1>")
+}
+
+async fn template_include() -> Html<&'static str> {
+    Html(include_str!("../templates/hello.html"))
+}
+#[derive(Template, Default)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
+}
+
+struct TemplateResponse<T>(T);
+
+impl<T> IntoResponse for TemplateResponse<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render templates. Error: {err}"),
+            )
+                .into_response(),
+        }
+    }
+}
+
+async fn askama_template(Path(name): Path<String>) -> impl IntoResponse {
+    let tpl = HelloTemplate { name };
+    TemplateResponse(tpl)
 }
