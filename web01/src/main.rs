27511c412677f6d951e23ca32a816b4a6f86861b
Yapi <hero.yp@qq.com>
2024-02-27 17:50:39 +0800

    feat(axum): middleware

--- web01/src/main.rs
+++ web01/src/main.rs
@@ -1,15 +1,27 @@
 mod db;
 
 use askama::Template;
-use axum::extract::Path;
+use axum::body::Body;
+use axum::extract::{Path, Request};
 use axum::response::{Html, IntoResponse, Response};
+use axum::routing::any_service;
 use axum::{
     http::StatusCode,
     routing::{get, post},
     Json, Router,
 };
 use dotenvy::dotenv;
+use http::HeaderName;
 use serde::{Deserialize, Serialize};
+use std::convert::Infallible;
+use std::time::Duration;
+use tokio::time::sleep;
+use tower::{service_fn, ServiceBuilder};
+use tower_http::compression::CompressionLayer;
+use tower_http::cors::{Any, CorsLayer};
+use tower_http::request_id::{MakeRequestUuid, SetRequestIdLayer};
+use tower_http::timeout::TimeoutLayer;
+use tower_http::trace::TraceLayer;
 use web01::api;
 
 #[tokio::main]
@@ -19,19 +31,65 @@ async fn main() {
     tracing_subscriber::fmt::init();
     let pool = db::establish_connection().await;
     // build our application with a route
-    let app = Router::new()
-        // 模版路径
+    let template_route = Router::new()
         .route("/tem1", get(template_str))
         .route("/tem2", get(template_string))
         .route("/tem3", get(template_include))
-        .route("/tem4/:name", get(askama_template))
-        .route("/api/login", post(api::users::login))
+        // askama template
+        .route("/tem4/:name", get(askama_template));
+
+    let api_route = Router::new()
+        .route("/login", post(api::users::login))
+        .route(
+            "/service02",
+            any_service(service_fn(|req: Request<Body>| async move {
+                let body = Body::from(format!("Hi from `{} {}` /", req.method(), req.uri()));
+                let res = Response::new(body);
+                Ok::<_, Infallible>(res)
+            })),
+        );
+
+    let app = Router::new()
+        // 模版路径
+        .route("/", get(index))
+        .nest("/tem", template_route)
+        .nest("/api", api_route)
+        .fallback(fallback)
+        .layer(
+            ServiceBuilder::new()
+                .layer(TraceLayer::new_for_http())
+                .layer(CompressionLayer::new())
+                // 超时时间
+                .layer(TimeoutLayer::new(Duration::new(0, 200000)))
+                // 设置请求头
+                .layer(SetRequestIdLayer::new(
+                    HeaderName::from_static("x-request-id"),
+                    MakeRequestUuid,
+                ))
+                .layer(CorsLayer::new().allow_origin(Any)),
+        )
         .with_state(pool);
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
         .unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn index(req: Request<Body>) -> String {
+    if rand::random() {
+        sleep(Duration::new(0, 300000)).await;
+    }
+
+    if let Some(req_id) = req.headers().get("x-request-id") {
+        format!("requestId[{:?}]", req_id)
+    } else {
+        String::from("none")
+    }
+}
+
+async fn fallback() -> String {
+    String::from("fallback")
 }
 
 async fn template_string() -> Html<String> {
