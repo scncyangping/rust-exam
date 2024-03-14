9124a4e386f87e221e860a406999b5f50954c593
Yapi <hero.yp@qq.com>
2024-03-14 19:16:46 +0800

    feat(match):  match partten.

--- stu/src/lib.rs
+++ stu/src/lib.rs
@@ -1,1 +1,37 @@
 pub mod cli_input;
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    enum MyEnum {
+        Foo,
+        Bar,
+    }
+    #[test]
+    fn test_fileter_enum() {
+        let v = vec![MyEnum::Foo, MyEnum::Bar];
+        let x: Vec<MyEnum> = v
+            .into_iter()
+            // 直接 == 判断会报错
+            //.filter(|x| x == MyEnum::Foo)
+            .filter(|x| matches!(x, MyEnum::Foo))
+            .collect();
+    }
+    struct TestMatchPartten {
+        A: String,
+        B: String,
+    }
+    /// 模式匹配
+    #[test]
+    fn test_match_pattern() {
+        let x: TestMatchPartten = TestMatchPartten {
+            A: String::from("value is a"),
+            B: String::from("value is b"),
+        };
+        let TestMatchPartten { A: c, B: d } = x;
+        // 模式匹配会移除所有权
+        // println!("a is {},b is {}", x.A, x.B);
+        println!("a is {},b is {}", c, d);
+    }
+}
