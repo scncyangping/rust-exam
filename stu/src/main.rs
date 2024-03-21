bb2eae9b369aded097294d6138f6c848ff1622c3
Yapi <hero.yp@qq.com>
2024-03-21 09:15:56 +0800

    feat(lifetime): static lifetime

--- stu/src/main.rs
+++ stu/src/main.rs
@@ -1,3 +1,25 @@
+use std::fmt::Debug;
+
+fn print_it2<T: Debug>(input: &'static T) {
+    println!("'static value passed in is: {:?}", input);
+}
+
+fn print_it<T: Debug + 'static>(input: &T) {
+    println!("'static value passed in is: {:?}", input);
+}
+#[derive(Debug)]
+struct TempRef<'a> {
+    temp: &'a String,
+}
+
 fn main() {
-    println!("main 1hello world")
+    let temp = String::from("temporary");
+    let temp_ref = TempRef { temp: &temp };
+    // temp_ref 不满足 'static 约束，因为它包含对局部 String 的引用
+    // 这行会导致编译错误
+    // 编译器会校验,传入的类型temp是否满足‘static。因为它有自己的生命
+    // 周期参数‘a，所以不是‘static。
+    //print_it(&temp_ref); 
+    static xxx: &str = "123";
+    print_it2(&xxx);
 }
