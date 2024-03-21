75ed9ffc5e39666b62647a47e6d1956c754bcf79
Yapi <hero.yp@qq.com>
2024-03-21 18:27:17 +0800

    feat(box): deref

--- stu/src/cccc/main1.rs
+++ stu/src/cccc/main1.rs
@@ -1,3 +1,12 @@
+use std::ops::Deref;
+
 fn main() {
-    println!("main 1hello world")
+    // 由于 String 实现了 Deref<Target=str>
+    let owned = "Hello".to_string();
+    // 因此下面的函数可以正常运行：
+    foo(&owned);
+}
+
+fn foo(s: &str) {
+    println!("{}", s);
 }
--- stu/src/main.rs
+++ stu/src/main.rs
@@ -1,25 +1,12 @@
-use std::fmt::Debug;
-
-fn print_it2<T: Debug>(input: &'static T) {
-    println!("'static value passed in is: {:?}", input);
-}
-
-fn print_it<T: Debug + 'static>(input: &T) {
-    println!("'static value passed in is: {:?}", input);
-}
-#[derive(Debug)]
-struct TempRef<'a> {
-    temp: &'a String,
-}
+use std::ops::Deref;
 
 fn main() {
-    let temp = String::from("temporary");
-    let temp_ref = TempRef { temp: &temp };
-    // temp_ref 不满足 'static 约束，因为它包含对局部 String 的引用
-    // 这行会导致编译错误
-    // 编译器会校验,传入的类型temp是否满足‘static。因为它有自己的生命
-    // 周期参数‘a，所以不是‘static。
-    //print_it(&temp_ref); 
-    static xxx: &str = "123";
-    print_it2(&xxx);
+    // 由于 String 实现了 Deref<Target=str>
+    let owned = "Hello".to_string();
+    // 因此下面的函数可以正常运行：
+    foo(&owned);
+}
+
+fn foo(s: &str) {
+    println!("{}", s);
 }
