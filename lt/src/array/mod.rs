f6aeed7906da66f1894cfa216dbe88529dc1d27c
scncyangping <hero.yp@qq.com>
2024-07-17 11:18:19 +0800

    fix(tss): version2

--- lt/src/array/mod.rs
+++ lt/src/array/mod.rs
@@ -16,14 +16,39 @@ fn two_split_search(arr: &[i32], target: i32) -> usize {
     return two_split_search(&arr[start..end], target) + start;
 }
 
+fn two_split_search_v2(arr: &[i32], target: i32) -> i32 {
+    // 左闭右开
+    let (mut left, mut right) = (0, arr.len());
+    while left < right {
+        let index = (left + right) / 2;
+        // 左闭右开
+        // 【0，1，3，4，5】 -》 5，
+        match arr[index].cmp(&target) {
+            std::cmp::Ordering::Less => left = index + 1,
+            std::cmp::Ordering::Equal => return index as i32,
+            std::cmp::Ordering::Greater => right = index,
+        }
+    }
+    -11
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::array::two_split_search_v2;
+
     use super::two_split_search;
 
     #[test]
     fn test_two_split() {
         let x = vec![-1, 0, 3, 5, 9, 12];
         let index = two_split_search(&x, 12);
+        println!("index: {index}")
+    }
+
+    #[test]
+    fn test_two_split_v2() {
+        let x = vec![-2, 0, 3, 5, 12];
+        let index = two_split_search_v2(&x, -1);
         println!("index: {index}")
     }
 }
